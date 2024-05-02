05df2f4648f3b65ede3d3700392acbf86518e800
RyuseiFC <ryu1008r@yahoo.co.jp>
2024-05-02 20:51:12 +0900

    ok

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,6 +1,5 @@
 import AuthGetCurrentUserServer from "@/components/AuthGetCurrentUserServer";
 import SignOutButton from "@/components/SignOutButton";
-import TodosServer from "@/components/TodosServer";
 
 export default async function Todos() {
   return (
